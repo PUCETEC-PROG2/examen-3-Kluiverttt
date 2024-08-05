11aac2a124c189e5dbe30ddd6d02fedfbad07828
Kluivert <kolopez@puce.edu.ec>
2024-08-05 17:58:23 -0500

    funcionalidades CRUD

--- album_manager/models.py
+++ album_manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):    
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    portada = models.ImageField(upload_to='portada/')
+
+    def __str__(self):
+        return self.title
--- album_manager/urls.py
+++ album_manager/urls.py
@@ -1,1 +1,17 @@
 # Ingresar tus URLs de la app aquí
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('artists/', views.artist_list, name='artist_list'),
+    path('artists/<int:pk>/', views.artist_detail, name='artist_detail'),
+    path('artists/new/', views.artist_create, name='artist_create'),
+    path('artists/<int:pk>/edit/', views.artist_update, name='artist_update'),
+    path('artists/<int:pk>/delete/', views.artist_delete, name='artist_delete'),
+
+    path('albums/', views.album_list, name='album_list'),
+    path('albums/<int:pk>/', views.album_detail, name='album_detail'),
+    path('albums/new/', views.album_create, name='album_create'),
+    path('albums/<int:pk>/edit/', views.album_update, name='album_update'),
+    path('albums/<int:pk>/delete/', views.album_delete, name='album_delete'),
+]
