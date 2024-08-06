d6ce178206637b9953c2629f02ef8becbf7a4322
Kluivert <kolopez@puce.edu.ec>
2024-08-05 21:14:19 -0500

    prueba final

--- album_manager/models.py
+++ album_manager/models.py
@@ -5,6 +5,7 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     
+    
     def __str__(self):
         return self.name
 
